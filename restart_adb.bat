df57128b4fd3e0c4b49555df3b1c7407a02e91cd
Skyward Studios <JoshBuckyy@gmail.com>
2025-03-03 16:35:03 +0000

    Delete restart_adb.bat

--- restart_adb.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Killing ADB server...
-X:\AndroidSDK\platform-tools\adb.exe kill-server
-
-echo Starting ADB server...
-X:\AndroidSDK\platform-tools\adb.exe start-server
-
-echo ADB server restarted successfully.
-pause
